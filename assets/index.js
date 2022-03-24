028f4a6e8ae7d01a8e03a860e3156f5f97bf2b27
ErinJordan222 <Erin.Jordan@du.edu>
2022-03-23 21:13:24 -0600

    adding_original_javascript_pt3

--- assets/index.js
+++ assets/index.js
@@ -113,4 +113,60 @@ let questions = [
 
     beginQuiz();
 
+    function restart() {
+        currentQuestion = 0;
+        nextBtn.classList.remove("hide");
+        submitBtn.classList.remove("hide");
+        trueBtn.classList.remove("hide");
+        falseBtn.classList.remove("hide");
+        score = 0;
+        userScore.innerHTML = score;
+        beginQuiz();
+    }
+
+    function next() {
+        currentQuestion++;
+        if(currentQuestion >= 5) {
+            nextBtn.classList.add("hide");
+        }
+        questionSection.innerHTML = questions[currentQuestion].question;
+        trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+        trueBtn.onclick = () => {
+            if(questions[currentQuestion].answers[0].answer) {
+                if(score < 6) {
+                    score++;
+                }
+            }
+
+            userScore.innerHTML = score;
+            if(currentQuestion < 5) {
+                next();
+            }
+        }
+
+falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+        falseBtn.onclick = () => {
+            if(questions[currentQuestion].answers[1].answer) {
+                if(score < 6) {
+                    score++;
+                }
+            }
+            userScore.innerHTML = score;
+            if(currentQuestion < 5) {
+                next();
+               
+
+            }
+        }
+    }
     
+    function submit() {
+        nextBtn.classList.add("hide");
+        submitBtn.classList.add("hide");
+        trueBtn.classList.add("hide");
+        falseBtn.classList.add("hide");
+        questionSection.classList.remove("hide");
+        questionSection.innerHTML ="Final Score = "
+        
+    }
+
